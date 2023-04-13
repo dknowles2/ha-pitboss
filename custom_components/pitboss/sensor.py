3f3fb5525518e802bb4a373e187d73b981a25aa3
David Knowles <dknowles2@gmail.com>
2023-04-13 23:28:27 +0000

    Add probe sensors

--- custom_components/pitboss/sensor.py
+++ custom_components/pitboss/sensor.py
@@ -1,45 +1,59 @@
 """Sensor platform for pitboss."""
 from __future__ import annotations
 
-from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
+from homeassistant.components.sensor import SensorDeviceClass, SensorEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import UnitOfTemperature
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
-from .coordinator import BlueprintDataUpdateCoordinator
-from .entity import IntegrationBlueprintEntity
-
-ENTITY_DESCRIPTIONS = (
-    SensorEntityDescription(
-        key="pitboss",
-        name="Integration Sensor",
-        icon="mdi:format-quote-close",
-    ),
-)
+from .coordinator import PitBossDataUpdateCoordinator
+from .entity import BaseEntity
 
 
-async def async_setup_entry(hass, entry, async_add_devices):
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_devices: AddEntitiesCallback
+):
     """Setup sensor platform."""
-    coordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_devices(
-        IntegrationBlueprintSensor(
-            coordinator=coordinator,
-            entity_description=entity_description,
-        )
-        for entity_description in ENTITY_DESCRIPTIONS
-    )
+    coordinator: PitBossDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+    entities = []
+    for i in range(1, coordinator.grill_spec.meat_probes + 1):
+        entities.append(ProbeSensor(coordinator, entry.unique_id, i))
+    async_add_devices(entities)
 
 
-class IntegrationBlueprintSensor(IntegrationBlueprintEntity, SensorEntity):
-    """pitboss Sensor class."""
+class ProbeSensor(BaseEntity, SensorEntity):
+    """PitBoss probe Sensor class."""
+
+    _attr_icon = "mdi:thermometer"
+    _attr_device_class = SensorDeviceClass.TEMPERATURE
 
     def __init__(
         self,
-        coordinator: BlueprintDataUpdateCoordinator,
-        entity_description: SensorEntityDescription,
+        coordinator: PitBossDataUpdateCoordinator,
+        entity_unique_id: str,
+        probe_number: int,
     ) -> None:
-        super().__init__(coordinator)
-        self.entity_description = entity_description
+        super().__init__(coordinator, entity_unique_id)
+        self._attr_unique_id = f"probe{probe_number}_{entity_unique_id}"
+        self._attr_name = f"Probe {probe_number}"
+        self._probe_number = probe_number
 
     @property
-    def native_value(self) -> str:
+    def native_unit_of_measurement(self) -> str | None:
+        if data := self.coordinator.data:
+            if not data["isFahrenheit"]:
+                return UnitOfTemperature.CELSIUS
+        return UnitOfTemperature.FAHRENHEIT
+
+    @property
+    def available(self) -> bool:
+        return bool(self.coordinator.data)
+
+    @property
+    def native_value(self) -> int | None:
         """Return the native value of the sensor."""
-        return self.coordinator.data.get("body")
+        if data := self.coordinator.data:
+            return data[f"p{self._probe_number}Temp"]
+        return None
