f226664042c91a6edaa246d8c6ad49886a3329d8
David Knowles <dknowles2@gmail.com>
2023-09-04 03:04:43 +0000

    Fix translation keys

--- custom_components/pitboss/switch.py
+++ custom_components/pitboss/switch.py
@@ -53,7 +53,7 @@ class PowerSwitch(BaseSwitchEntity):
 
     entity_description = SwitchEntityDescription(
         key="moduleIsOn",
-        translation_key="moduleIsOn",
+        translation_key="module_is_on",
         device_class=SwitchDeviceClass.SWITCH,
     )
 
@@ -71,7 +71,7 @@ class PrimerSwitch(BaseSwitchEntity):
 
     entity_description = SwitchEntityDescription(
         key="primeState",
-        translation_key="primeState",
+        translation_key="prime_state",
         device_class=SwitchDeviceClass.SWITCH,
     )
 
