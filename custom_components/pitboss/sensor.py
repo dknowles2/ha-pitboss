0e4e89e3e32f0903a312fd3841c1314e3256b90d
David Knowles <dknowles2@gmail.com>
2023-09-04 01:07:10 +0000

    Run black

--- custom_components/pitboss/sensor.py
+++ custom_components/pitboss/sensor.py
@@ -1,7 +1,11 @@
 """Sensor platform for pitboss."""
 from __future__ import annotations
 
-from homeassistant.components.sensor import SensorDeviceClass, SensorEntity, SensorStateClass
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorStateClass,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import UnitOfTemperature
 from homeassistant.core import HomeAssistant
